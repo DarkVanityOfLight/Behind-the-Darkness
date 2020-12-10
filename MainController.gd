ced2c7b8bbc80b98c9eaf8a256b2a9b6bd2af5df
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-12-10 23:15:04 +0100

    chore: assets

---
+++ MainController.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const actions = []
+const situations = [
+	"At the lake (Not looked at shadow)",
+	"At the lake (Looked at shadow)"]
+
+var sanity: int = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+

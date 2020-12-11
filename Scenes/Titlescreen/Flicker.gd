00d2001b874c05131929fded3f0c818d21ebe2d1
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-12-11 14:51:24 +0100

    chore: change durations and co

--- Scenes/Titlescreen/Flicker.gd
+++ Scenes/Titlescreen/Flicker.gd
@@ -5,18 +5,19 @@ onready var noise = OpenSimplexNoise.new()
 var value = 0.0
 const MAX_VALUE = 10000000000
 
+export var period = 50
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	value = 0
-	noise.period = 20
+	noise.period = 50
 	
 func _physics_process(delta):
 	value += 0.5
 	if (value > MAX_VALUE):# When the value gets too big it looses percision
 		value = 0.0# And stops generating
-	var alpha =  ((noise.get_noise_1d(value) + 1)/4) + 0.5
+	var alpha =  ((noise.get_noise_1d(value) + 1)/4)
 	self.color = Color(color.r, color.g, color.b, alpha)
 
 

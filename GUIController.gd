62459d6dfa8c91c66825c936973468ccbefa665b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-12-11 15:09:26 +0100

    chore: change connection name

--- GUIController.gd
+++ GUIController.gd
@@ -2,7 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	MainController.connect("new_situation", self, "_on_Button_pressed")
+	MainController.connect("new_situation", self, "_on_MainController_scene_changed")
 
-func _on_Button_pressed():
+func _on_MainController_scene_changed():
 	print("Situation changed")

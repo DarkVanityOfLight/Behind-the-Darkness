12e06d29eaed475eb26aa6ff631cad1935028af5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-12-12 11:58:24 +0100

    feat: Implement transitions

--- GUIController.gd
+++ GUIController.gd
@@ -2,6 +2,9 @@ extends Node
 
 var title_screen = preload("res://Scenes/Titlescreen/Node2D.tscn")
 var first_lvl = preload("res://Scenes/Lvl1/Lvl1.tscn")
+var second_lvl = preload("res://Scenes/Lvl2/Lvl2.tscn")
+var third_lvl = preload("res://Scenes/Lvl3/Lvl3.tscn")
+var fourth_lvl = preload("res://Scenes/Lvl4/Lvl4.tscn")
 onready var current_scene = title_screen.instance()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +12,20 @@ func _ready():
 	current_scene.get_node("gaian/Button").connect("pressed", self, "_start_game")
 	MainController.connect("new_situation", self, "_on_MainController_scene_changed")
 
-func _on_MainController_scene_changed():
-	print("Situation changed")
 
+func _on_MainController_scene_changed():
+	var current_situation = MainController.current_situation
+	remove_child(current_scene)
+	if current_situation >= 3 and current_situation <= 6:
+		current_scene = second_lvl.instance()
+	elif current_situation >= 7 and current_scene <= 9:
+		current_scene = third_lvl.instance()
+	elif current_scene >= 10 and current_scene <= 27:
+		current_scene = fourth_lvl.instance()
+		
+	add_child(current_scene)
+	
+	
 func _start_game():
 	remove_child(current_scene)
 	current_scene = first_lvl.instance()

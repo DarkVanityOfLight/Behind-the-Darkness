fa2f9f6c4ee2f55c80efdae803b4ad909e862765
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-12-12 14:30:40 +0100

    feat: add sanity bar

--- GUIController.gd
+++ GUIController.gd
@@ -5,13 +5,15 @@ var first_lvl = preload("res://Scenes/Lvl1/Lvl1.tscn")
 var second_lvl = preload("res://Scenes/Lvl2/Lvl2.tscn")
 var third_lvl = preload("res://Scenes/Lvl3/Lvl3.tscn")
 var fourth_lvl = preload("res://Scenes/Lvl4/Lvl4.tscn")
+var sanity_gui_scene = preload("res://Scenes/SanityGUI/Sanity.tscn")
 onready var current_scene = title_screen.instance()
+var sanity_gui = sanity_gui_scene.instance()
+var san_holder = CanvasLayer.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(current_scene)
 	current_scene.get_node("gaian/Button").connect("pressed", self, "_start_game")
 	MainController.connect("new_situation", self, "_on_MainController_scene_changed")
-
 
 func _on_MainController_scene_changed():
 	var current_situation = MainController.current_situation
@@ -25,9 +27,9 @@ func _on_MainController_scene_changed():
 		
 	add_child(current_scene)
 	
-	
 func _start_game():
 	remove_child(current_scene)
 	current_scene = first_lvl.instance()
 	add_child(current_scene)
-	
+	san_holder.add_child(sanity_gui)
+	add_child(san_holder)
